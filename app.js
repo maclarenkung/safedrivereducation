eedcdd2d9533dfb79aa69b61114fda7b0fc6c720
KG <maclarenkung@gmail.com>
2020-02-18 15:59:56 +0700

    mobile

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 const lines = document.querySelectorAll(".hamburger .lines");
 
+const logo = document.querySelectorAll(".logo-top");
+const ham = document.querySelectorAll(".ham");
+
 hamburger.addEventListener("click", () => {
   lines.forEach(lines => {
     lines.classList.toggle("white");
@@ -10,5 +13,11 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   links.forEach(link => {
     link.classList.toggle("fades");
+  });
+  logo.forEach(logo => {
+    logo.classList.toggle("logonone");
+  });
+  ham.forEach(ham => {
+    ham.classList.toggle("ham-top");
   });
 });
